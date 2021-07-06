8e888ea6bb3c599f3f8d63602d02191f65af4789
michaelgoodin2045 <michaelgoodin@gmail.com>
2021-07-06 15:21:34 -0400

    initial commit of loopstudios project

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let navbar = document.querySelector('.navbar');
+let hamburger = document.querySelector('.hamburger');
+
+function navbarDisplayToggle() {
+    if (navbar.style.display === 'block') {
+        navbar.style.display = 'none';
+    } else {
+        navbar.style.display = 'block';
+    }
+    hamburger.classList.toggle('toggle');
+}
+
+hamburger.addEventListener('click', navbarDisplayToggle);
