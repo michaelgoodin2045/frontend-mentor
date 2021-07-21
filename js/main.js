4e10443a3ec69ed6b6163785589be94872825f37
Michael Goodin <michaelgoodin@gmail.com>
2021-07-21 16:26:27 -0400

    Layout fixes and added SASS directory to GitHub

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ let navbar = document.querySelector('.navbar');
 let hamburger = document.querySelector('.hamburger');
 
 function navbarDisplayToggle() {
-    if (navbar.style.display === 'block') {
-        navbar.style.display = 'none';
-    } else {
-        navbar.style.display = 'block';
-    }
-    hamburger.classList.toggle('toggle');
+  if (navbar.style.display === 'block') {
+    navbar.style.display = 'none';
+  } else {
+    navbar.style.display = 'block';
+  }
+  hamburger.classList.toggle('toggle');
 }
 
 hamburger.addEventListener('click', navbarDisplayToggle);
